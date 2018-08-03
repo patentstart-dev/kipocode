d474f826edb9cff4cb35504ab5eb646bf0d739a4
patentstart-dev <31948826+patentstart-dev@users.noreply.github.com>
2018-08-03 17:54:28 +0900

    Update kipocode.py

--- kipocode/kipocode.py
+++ kipocode/kipocode.py
@@ -15,17 +15,20 @@ def kipocode_get(person_type, socialcode):
     # 드라이버 접속을 위한 option 설정
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
-    options.add_argument('window-size=1920x1080')
-    options.add_argument('disable-gpu')
-    options.add_argument('--ignore-certificate-errors')
+    # options.add_argument('window-size=1920x1080')
+    # options.add_argument("disable-gpu")
 
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     ROOT_DIR = os.path.dirname(BASE_DIR)
     CHROME_DIR = os.path.join(BASE_DIR,'chromedriver')
 
+    print(CHROME_DIR)
+
     # headless 크롬 드라이버를 이용한 접속
     driver = webdriver.Chrome(CHROME_DIR, chrome_options=options)
-    
+    #driver = webdriver.Chrome(chrome_options=options)
+
+
     # url에 접근한다.
     driver.get('https://www.patent.go.kr/jsp/ka/prestep/codeapp/CodeAppView.do')
 
@@ -60,33 +63,41 @@ def kipocode_get(person_type, socialcode):
         # 실행버튼
         driver.execute_script("apagtCdCheck(); return false;")
 
+
         # 팝업 윈도우 이동
-        window_child = driver.window_handles[1]
-               
+        # window_child=window_before+".0.child"
+        window_child=driver.window_handles[1]
+        
         try:
             wait3 = WebDriverWait(driver, 1)
             element3 = wait3.until(EC.visibility_of_element_located((By.ID, 'pop_content')))
             print("switch wait")
         except:
             print("switch non wait")
-        
-        driver.switch_to.window(window_child)
-        
-        print("switch success")
 
-               
-        
+
+        driver.switch_to.window(window_child)
+
         # 크롤링
         html=driver.page_source
         soup = BeautifulSoup(html, 'html.parser')
 
+        #rint(html)
+        #rint(soup)
+
         try:
             kipocode = soup.find(class_='txtPointS').get_text().strip()
+            print(soup.find(class_='txtPointS'))
         except:
             print("KIPOCODE None")
 
         # 모든 윈도우 종료
         driver.quit()
+
+        #driver.switch_to.window(window_before)
+        #driver.close()
+
+        print(kipocode)
 
         return kipocode
 
@@ -111,18 +122,18 @@ def kipocode_get(person_type, socialcode):
         driver.execute_script("apagtCdCheck(); return false;")
 
         # 팝업 윈도우 이동
-        window_child = driver.window_handles[1]
-               
+        # window_child=window_before+".0.child"
+        window_child=driver.window_handles[1]
+
         try:
             wait3 = WebDriverWait(driver, 1)
             element3 = wait3.until(EC.visibility_of_element_located((By.ID, 'pop_content')))
             print("switch wait")
         except:
             print("switch non wait")
-        
+
+
         driver.switch_to.window(window_child)
-        
-        print("switch success")
 
         # 크롤링
         html=driver.page_source
@@ -133,10 +144,13 @@ def kipocode_get(person_type, socialcode):
         except:
             print("KIPOCODE None")
 
-        # 모든 윈도우 종료
-        driver.quit()
+        print(kipocode)
 
         return kipocode
 
 
     return "person_type error"
+
+
+
+        
